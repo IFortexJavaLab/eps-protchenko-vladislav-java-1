ece0e14d2e69b6757eb68331a0019c74a5d0a5e3
Vladislav Protchenko <vladislav.protchenko@ifortex.com>
2024-10-30 14:41:32 +0300

    EPS-19: remove unnecessary dependencies and lombok annotations

--- src/main/java/com/ifortex/internship/model/Course.java
+++ src/main/java/com/ifortex/internship/model/Course.java
@@ -1,17 +1,12 @@
 package com.ifortex.internship.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Set;
+import lombok.Builder;
+import lombok.Data;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class Course {
 
--- src/main/java/com/ifortex/internship/model/Student.java
+++ src/main/java/com/ifortex/internship/model/Student.java
@@ -1,13 +1,9 @@
 package com.ifortex.internship.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class Student {
 
