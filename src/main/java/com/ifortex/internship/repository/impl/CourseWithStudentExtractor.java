4346eb6c6d2cec114d250436276b851c0f494ae5
Vladislav Protchenko <vladislav.protchenko@ifortex.com>
2024-11-05 22:02:55 +0300

    EPS-20: add CourseWithStudentExtractor

---
+++ src/main/java/com/ifortex/internship/repository/impl/CourseWithStudentExtractor.java
@@ -0,0 +1,46 @@
+package com.ifortex.internship.repository.impl;
+
+import com.ifortex.internship.model.Course;
+import com.ifortex.internship.model.Student;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.*;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseWithStudentExtractor implements ResultSetExtractor<List<Course>> {
+
+  @Override
+  public List<Course> extractData(ResultSet rs) throws SQLException, DataAccessException {
+    Map<Integer, Course> courseMap = new HashMap<>();
+
+    while (rs.next()) {
+      int courseId = rs.getInt("course_id");
+      Course course = courseMap.get(courseId);
+      if (course == null) {
+        course =
+            Course.builder()
+                .id(courseId)
+                .name(rs.getString("course_name"))
+                .description(rs.getString("course_description"))
+                .price(rs.getBigDecimal("course_price"))
+                .duration(rs.getInt("course_duration"))
+                .startDate(rs.getObject("course_start_date", LocalDateTime.class))
+                .lastUpdateDate(rs.getObject("course_last_update_date", LocalDateTime.class))
+                .isOpen(rs.getBoolean("course_is_open"))
+                .students(new HashSet<>())
+                .build();
+        courseMap.put(course.getId(), course);
+      }
+
+      Student student =
+          Student.builder().id(rs.getInt("student_id")).name(rs.getString("student_name")).build();
+      course.getStudents().add(student);
+    }
+
+    return new ArrayList<>(courseMap.values());
+  }
+}
